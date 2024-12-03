4d7ad1ab37a3036c5f990375b56e5b1cebcb7aac
Khaled <khaledrafat04@yahoo.com>
2024-12-03 02:35:20 +0200

    First

---
+++ STCGitApp/STCGitApp/STCGitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  STCGitAppApp.swift
+//  STCGitApp
+//
+//  Created by Khaled on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct STCGitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
